4712b606a4a152c2285a27c8d53f51462231e268
jim <jimshawster@gmail.com>
2022-02-09 19:08:48 -0500

    fixed read de-dup issue on consensus alleles

--- scripts/get_bam_partition.py
+++ scripts/get_bam_partition.py
@@ -1,4 +1,5 @@
 import pysam
+import subprocess
 import sys
 read_part_file = sys.argv[1]
 bam_file = sys.argv[2]
@@ -18,18 +19,31 @@ for line in open(read_part_file,'r'):
 ploidy = len(read_part)
 
 obam_files = []
-
+file_names = []
 
 for i in range(ploidy):
-    obam_files.append(pysam.AlignmentFile(pref_nam+str(i)+".bam", "wb", template=bam))
+    file_name = pref_nam+str(i)+".bam"
+    file_names.append(file_name)
+    obam_files.append(pysam.AlignmentFile(file_name, "wb", template=bam))
+
+file_names.append(pref_nam+"-not_mapped.bam")
+obam_files.append(pysam.AlignmentFile(pref_nam+"-not_mapped.bam", "wb", template=bam))
 
 for b in bam.fetch(until_eof=True):
+    not_frag = True;
     for i in range(ploidy):
         qnames = read_part[i]
         if b.query_name in qnames:
             obam_files[i].write(b)
+            not_frag=False
+    if not_frag:
+        obam_files[-1].write(b);
 
 
 for obam in obam_files:
     obam.close()
+
+for file_name in file_names:
+    subprocess.run("samtools index " + file_name, shell=True, check=True)
+
 bam.close()
